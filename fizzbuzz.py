17e9a6e6d3559e6243d6df45f1add9a468d73106
stevetech73 <stevetech18@gmail.com>
2024-11-03 18:38:43 -0500

    Completed assignment from Steve Randall

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,18 @@
 # add your code here
+count = 0
 
+for count in range(1, 101):
+    
+    if count > 100:
+        print("Done")
+        exit()
+    else:    
+        if count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print ("Buzz")
+        elif count % 3 == 0 and count % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(count)
+        
