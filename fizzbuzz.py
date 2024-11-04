44dd14bcc34163c373610c90c5684836c7670489
stevetech73 <stevetech18@gmail.com>
2024-11-04 15:32:25 -0500

    Corrected Stev Randall fizzbuzz assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,19 @@
 # add your code here
-count = 0
+count = 0      # set counter
 
-for count in range(1, 101):
+for count in range(1, 101):      # define the range
     
-    if count > 100:
-        print("Done")
-        exit()
-    else:    
-        if count % 3 == 0:
+    if count > 100:       # count is greater than 100
+        print("Done")       
+        exit()        # stop
+    else:
+        if count % 3 == 0 and count % 5 == 0:         # number is divisible by 3 and by 5
+            print("FizzBuzz")    
+        elif count % 3 == 0:       # number is divisible by 3
             print("Fizz")
-        elif count % 5 == 0:
+        elif count % 5 == 0:      # number is divisible by 5
             print ("Buzz")
-        elif count % 3 == 0 and count % 5 == 0:
-            print("FizzBuzz")
+                
         else:
             print(count)
         
